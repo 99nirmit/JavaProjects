e86ceb0ead0d61f8cc72a32a98a9a7c0e22afe24
99nirmit <79360867+99nirmit@users.noreply.github.com>
2025-03-22 17:05:31 +0530

    upload & retrieve file is added

--- employeemanagementsystem/src/main/java/com/employeemanagementapp/employeemanagementsystem/controller/EmployeeController.java
+++ employeemanagementsystem/src/main/java/com/employeemanagementapp/employeemanagementsystem/controller/EmployeeController.java
@@ -3,8 +3,13 @@ package com.employeemanagementapp.employeemanagementsystem.controller;
 import com.employeemanagementapp.employeemanagementsystem.models.Employee;
 import com.employeemanagementapp.employeemanagementsystem.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -14,7 +19,7 @@ public class EmployeeController {
     private EmployeeService employeeService;
 
     @PostMapping("/save")
-    private Employee addEmployee(@RequestBody Employee employee){
+    private Employee addEmployee(@RequestBody Employee employee) {
         return employeeService.addEmployee(employee);
     }
 
@@ -22,31 +27,47 @@ public class EmployeeController {
     private Page<Employee> getAllEmployee(@RequestParam(defaultValue = "0") int page,
                                           @RequestParam(defaultValue = "10") int size,
                                           @RequestParam(defaultValue = "id") String sortBy,
-                                          @RequestParam(defaultValue = "asc") String order){
+                                          @RequestParam(defaultValue = "asc") String order) {
         return employeeService.getAllEmployee(page, size, sortBy, order);
     }
 
     @GetMapping("/{id}")
-    private Employee getEmployee(@PathVariable Long id){
+    private Employee getEmployee(@PathVariable Long id) {
         return employeeService.getEmployee(id);
     }
 
     @PutMapping("/update/{id}")
-    private Employee updateEmployee(@PathVariable Long id, @RequestBody Employee updateEmployee){
+    private Employee updateEmployee(@PathVariable Long id, @RequestBody Employee updateEmployee) {
         return employeeService.updateEmployee(id, updateEmployee);
     }
 
     @DeleteMapping("/delete/{id}")
-    private void deleteEmployee(@PathVariable Long id){
+    private void deleteEmployee(@PathVariable Long id) {
         employeeService.deleteEmployee(id);
     }
 
     @GetMapping("/department")
     private Page<Employee> getEmployeeByDepartment(@RequestParam String department,
                                                    @RequestParam(defaultValue = "0") int page,
-                                                   @RequestParam(defaultValue = "10") int size){
-        return employeeService.getEmployeeByDepartment(department,page, size);
-
+                                                   @RequestParam(defaultValue = "10") int size) {
+        return employeeService.getEmployeeByDepartment(department, page, size);
     }
 
+    @PostMapping("/{id}/upload")
+    private ResponseEntity<String> uploadFile(@PathVariable Long id, @RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return ResponseEntity.badRequest().body("File is Empty");
+        }
+        return employeeService.uploadFile(id, file);
+    }
+
+    @GetMapping("/{id}/files")
+    private ResponseEntity<List<String>> getFilesByEmployeeId(@PathVariable Long id){
+        return employeeService.getFilesByEmployeeName(id);
+    }
+
+    @GetMapping("/{id}/download/{filename}")
+    private ResponseEntity<Resource> downloadFile(@PathVariable Integer id, @PathVariable String filename){
+        return employeeService.downloadFile(id, filename);
+    }
 }
--- employeemanagementsystem/src/main/java/com/employeemanagementapp/employeemanagementsystem/service/EmployeeService.java
+++ employeemanagementsystem/src/main/java/com/employeemanagementapp/employeemanagementsystem/service/EmployeeService.java
@@ -3,15 +3,32 @@ package com.employeemanagementapp.employeemanagementsystem.service;
 import com.employeemanagementapp.employeemanagementsystem.models.Employee;
 import com.employeemanagementapp.employeemanagementsystem.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
 public class EmployeeService {
+
+    private static final String UPLOAD_DIR = "E:/Files/";
 
     @Autowired
     private EmployeeRepository employeeRepository;
@@ -49,5 +66,66 @@ public class EmployeeService {
     public Page<Employee> getEmployeeByDepartment(String department, int page, int size){
         Pageable pageable1 = PageRequest.of(page, size);
         return employeeRepository.findByDepartment(department, pageable1);
+    }
+
+    public ResponseEntity<String> uploadFile(Long id, MultipartFile file){
+
+        try{
+            File uploadDir = new File(UPLOAD_DIR);
+            if(uploadDir.exists()){
+                uploadDir.mkdirs();
+            }
+            File destinationFile =  new File(UPLOAD_DIR + id + "_" + file.getOriginalFilename());
+            file.transferTo(destinationFile);
+
+            return ResponseEntity.ok("File Upload Successfully: " + destinationFile.getAbsolutePath());
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("File Upload failed: " + e.getMessage());
+        }
+    }
+
+    public ResponseEntity<List<String>> getFilesByEmployeeName(Long id){
+        File uploadDir = new File(UPLOAD_DIR);
+
+        if(!uploadDir.exists() || !uploadDir.isDirectory()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Collections.singletonList("No Files Found"));
+        }
+
+        File[] files = uploadDir.listFiles(((dir, name) -> name.startsWith(id + "_")));
+
+        if(files == null || files.length == 0){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Collections.singletonList("No files found for employee ID:" + id));
+        }
+
+        List<String> filesNames = Arrays.stream(files)
+                .map(File::getName)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(filesNames);
+    }
+
+    public ResponseEntity<Resource> downloadFile(Integer id, String filename){
+        try{
+            Path filePath = Paths.get("Files").resolve(filename).normalize();
+
+            if(!Files.exists(filePath)){
+                throw new RuntimeException("File Not found: " + filename);
+            }
+            Resource resource = new UrlResource(filePath.toUri());
+
+            if(!resource.exists() || !resource.isReadable()){
+                throw new RuntimeException("File is not readable: " + filename);
+            }
+
+
+                return ResponseEntity.ok()
+                        .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; fileName=\"" + filename + "\"")
+                        .body(resource);
+
+            }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(null);
+        }
     }
 }
